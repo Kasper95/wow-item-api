8e378dac01f91da3bcd054de83bdfd1bc25bc405
kasper95 <kove_kasper@outlook.com>
2017-05-21 09:20:07 -0400

    initial commit

---
+++ src/main/java/com/kasperskove/wowitemapi/WowItemApiApplication.java
@@ -0,0 +1,12 @@
+package com.kasperskove.wowitemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowItemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowItemApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kasperskove/wowitemapi/entities/Item.java
@@ -0,0 +1,11 @@
+package com.kasperskove.wowitemapi.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "items")
+public class Item {
+
+
+}
